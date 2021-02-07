fd72f0ded098201ce06305d3cc7c08776b542c32
Maksim_Andreev1 <maksim_andreev1@epam.com>
2021-02-07 21:49:24 +0300

    Add hello world endpoint

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,33 +1,13 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
-import java.math.BigDecimal;
-import java.util.concurrent.Callable;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootConfiguration
+@SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-        if (args.length != 2) {
-            throw new UnsupportedOperationException("Operation unsupported");
-        }
-
-        BigDecimal result = handleException(() -> addition(args[0], args[1]));
-
-        System.out.println("Result: " + result);
     }
 
-    private static BigDecimal addition(String arg1, String arg2) {
-        return new BigDecimal(arg1).add(new BigDecimal(arg2));
-    }
-
-    private static BigDecimal handleException(Callable<BigDecimal> callable) {
-        try {
-            return callable.call();
-        } catch (Exception any) {
-            throw new RuntimeException("Error on operation", any);
-        }
-    }
 }
